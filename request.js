1482f3a899a4781f1f7a98dc566694e563c645a0
Debbs1 <62051244+Debbs1@users.noreply.github.com>
2020-08-01 22:09:45 +0100

    Update request.js

--- request.js
+++ request.js
@@ -1,6 +1,6 @@
 const key="fb80d66964d0ba43d985bb2020622826";
 const requestCity = async (city)=>{
-	const baseURL='http://api.openweathermap.org/data/2.5/weather';
+	const baseURL='https://api.openweathermap.org/data/2.5/weather';
 	const query = '?q='+city+'&appid='+key;
 
 	const response = await fetch(baseURL + query);
